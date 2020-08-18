ad6b7b966bc039a39ec194f9de3889ecc49525c4
Nigmat005 <Nigmathalmamat@gmail.com>
2020-08-18 18:18:53 -0400

    Test 1 class

--- src/day1/Test1.java
+++ src/day1/Test1.java
@@ -5,5 +5,6 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("Done");
         System.out.println("Add something");
+        System.out.println("Change");
     }
 }
